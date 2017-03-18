b684950986ecf706b323b850d9c214b57e9d9617
James Moore <jameslmoore@james-moores-macbook.local>
2017-03-18 13:40:18 -1000

    pulling price data into  dataframe

--- pricedata.py
+++ pricedata.py
@@ -1,2 +1,15 @@
 import datetime as dt
 import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+start = dt.datetime(2015,1,1)
+stop = dt.datetime(2017,2,28)
+
+df = web.DataReader('TNP', 'yahoo', start, stop)
+
+
+print(df.tail())
