41abf100cdb5c6c0ddb61991ab9ab741868dba3c
James Moore <jameslmoore@james-moores-macbook.local>
2017-03-18 13:46:00 -1000

    convert web call to csv

--- pricedata.py
+++ pricedata.py
@@ -4,12 +4,14 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
-style.use('ggplot')
+##style.use('ggplot')
+##
+##start = dt.datetime(2015,1,1)
+##stop = dt.datetime(2017,2,28)
+##
+##df = web.DataReader('TNP', 'yahoo', start, stop)
+##print(df.tail())
+##df.to_csv('tnp.csv')
 
-start = dt.datetime(2015,1,1)
-stop = dt.datetime(2017,2,28)
-
-df = web.DataReader('TNP', 'yahoo', start, stop)
-
-
+df = pd.read_csv('tnp.csv')
 print(df.tail())
