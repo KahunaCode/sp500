da256f5133c2a4c3819858c95b87fb38c42e673c
James Moore <jameslmoore@james-moores-macbook.local>
2017-03-19 13:32:29 -1000

    pull stock data from yahoo

--- sp500tickers.py
+++ sp500tickers.py
@@ -1,6 +1,10 @@
 import bs4 as bs
 import pickle
 import requests
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 
 def sp500_tickers():
     response = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
@@ -18,5 +22,26 @@ def sp500_tickers():
 
     return tickers
 
-sp500_tickers()
-    
+#sp500_tickers()
+
+def get_data(reload=False):
+    if reload:
+        tickers = sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+    if not os.path.exists('stocks'):
+        os.makedirs('stocks')
+
+    start = dt.datetime(2010,1,1)
+    stop = dt.datetime(2016,12,31)
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stocks/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, stop)
+            df.to_csv('stocks/{}.csv'.format(ticker))
+        else:
+            print('{} exists'.format(ticker))
+
+get_data()
