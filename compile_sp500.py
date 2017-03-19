a193d6e05e4b81574c0cf06dcc281063ab43ab8f
James Moore <jameslmoore@james-moores-macbook.local>
2017-03-19 13:49:17 -1000

    finished adding all csv into pandas dataframe and output csv

--- compile_sp500.py
+++ compile_sp500.py
@@ -11,10 +11,18 @@ def compile_tickers():
         df = pd.read_csv('stocks/{}.csv'.format(ticker))
         df.set_index('Date', inplace=True)
 
-        df.rename(columns = {'Adj Close', ticker}, inplace=True)
+        df.rename(columns = {'Adj Close':ticker}, inplace=True)
         df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
 
         if df_all.empty:
             df_all = df
         else:
             df_all = df_all.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(df_all.head())
+    df_all.to_csv('sp500_all.csv')
+
+compile_tickers()
