aaf58c03d452bf3e913d63bcb6e706c27d6c8709
James Moore <jameslmoore@james-moores-macbook.local>
2017-03-19 13:41:40 -1000

    pull CSV data into one pandas df

---
+++ compile_sp500.py
@@ -0,0 +1,20 @@
+import pickle
+import pandas as pd
+
+def compile_tickers():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    df_all = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stocks/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns = {'Adj Close', ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if df_all.empty:
+            df_all = df
+        else:
+            df_all = df_all.join(df, how='outer')
