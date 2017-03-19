a41de3eadf808c098d9848b6baf076f8b71d4f4c
James Moore <jameslmoore@james-moores-macbook.local>
2017-03-18 14:17:00 -1000

    plt price, moving avg, volume

--- pricedata.py
+++ pricedata.py
@@ -25,3 +25,11 @@ df['100_moving_avg'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
 #df.dropna(inplace=True)#drops NaN's without redoing df
 print(df.head())
 
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100_moving_avg'])
+ax2.bar(df.index, df['Volume'])
+
+plt.show()
